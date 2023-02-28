261c625cc826772c1fe6336fc214403eb3743a6c
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2023-02-27 21:04:45 -0500

    app set up done, data coming through

---
+++ src/components/Posts.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Posts = ({ posts, loading }) => {
+  
+  if(loading) {
+      return <h2>Loading</h2>
+  }
+
+  return (
+    <ul className="list-group mb4">
+        {posts.map(post => (
+            <li k
+            key={post.id}
+            className="list-group-item">
+                {post.title}
+            </li>
+        ))}
+      
+    </ul>
+  )
+}
+
+export default Posts
