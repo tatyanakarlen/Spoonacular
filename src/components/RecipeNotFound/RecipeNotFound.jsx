0ab2674f92ce8606213f91d41829d8fe9acfdec0
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2023-03-12 15:25:23 -0400

    finished recipe not found page

--- src/components/RecipeNotFound/RecipeNotFound.jsx
+++ src/components/RecipeNotFound/RecipeNotFound.jsx
@@ -1,44 +1,54 @@
 import MobileNav from '../MobileNav/MobileNav';
 import LogoSocialLinks from '../LogoSocialLinks/LogoSocialLinks';
+import { Link } from 'react-router-dom';
 import './RecipeNotFound.css';
+import img from '../../Assets/404-error.png';
 
 const RecipeNotFound = () => {
   return (
     <>
       <LogoSocialLinks />
       <div className="recipe-not-found">
-        <h1>Recipe not found</h1>
-        <>
-          Unfortunatley CookBook couldn't find any recipes for you. Try these
-          options for better luck:
-        </>
-        <ul>
-          <li>Africa</li>
-          <li>American</li>
-          <li>British</li>
-          <li>Cajun</li>
-          <li>Caribbean</li>
-          <li>Chinese</li>
-          <li>Eastern European</li>
-          <li>French</li>
-          <li>German</li>
-          <li>Greek</li>
-          <li>Indian</li>
-          <li>Irish</li>
-          <li>Italian</li>
-          <li>Japanese</li>
-          <li>Jewish</li>
-          <li>Korean</li>
-          <li>Latin American</li>
-          <li>Mediterranean</li>
-          <li>Mexican</li>
-          <li>Middle Eastern</li>
-          <li>Nordic</li>
-          <li>Souther</li>
-          <li>Spanish</li>
-          <li>Thai</li>
-          <li>Vietnamese</li>
-        </ul>
+        <img src={img} />
+        <div class="recipe-not-found-h1-go-home-link">
+          <h1>Recipes not found</h1>
+        </div>
+
+        <p>
+          Oops! CookBook couldn't find any recipes for you. Head{' '}
+          <Link>home</Link> and try these options:
+        </p>
+        <div class="ul-container">
+          <ul>
+            <li>African</li>
+            <li>American</li>
+            <li>British</li>
+            <li>Cajun</li>
+            <li>Caribbean</li>
+            <li>Chinese</li>
+            <li>Eastern European</li>
+            <li>French</li>
+            <li>German</li>
+            <li>Greek</li>
+            <li>Indian</li>
+            <li>Irish</li>
+            <li>Italian</li>
+          </ul>
+          <ul>
+            <li>Japanese</li>
+            <li>Jewish</li>
+            <li>Korean</li>
+            <li>Latin American</li>
+            <li>Mediterranean</li>
+            <li>Mexican</li>
+            <li>Middle Eastern</li>
+            <li>Nordic</li>
+            <li>Souther</li>
+            <li>Spanish</li>
+            <li>Thai</li>
+            <li>Vietnamese</li>
+          </ul>
+        </div>
       </div>
     </>
   );
