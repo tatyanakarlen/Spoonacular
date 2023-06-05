7564e9f907606a74edb620d1200981d86ae9e1ed
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2023-06-05 13:26:46 -0400

    sign-in container styling done

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,5 +1,5 @@
 import { auth, googleProvider } from '../../config/firebase-config';
-import './Auth.css'
+import './Auth.css';
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -18,14 +18,12 @@ import {
   MDBModalFooter,
 } from 'mdb-react-ui-kit';
 
-
 const Auth = () => {
   const [centredModal, setCentredModal] = useState(false);
   const toggleShow = () => setCentredModal(!centredModal);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  
   //   console.log(auth?.currentUser?.photoURL);
   // photo URL if user is signed in with Gooogle
 
@@ -64,63 +62,84 @@ const Auth = () => {
   return (
     <div className="auth-page-container">
       <div className="sign-in">
-      
         <h1>Welcome!</h1>
         <h5>Continue with Google or sign-in manually</h5>
-        
-        <button><img width="27" height="27" src="https://img.icons8.com/color/48/google-logo.png" alt="google-logo"/>Log in with Google</button>
+
+        <button class="log-in-with-google-btn" onClick={signInWithGoogle}>
+          <img
+            width="20"
+            height="20"
+            src="https://img.icons8.com/color/48/google-logo.png"
+            alt="google-logo"
+          />
+          Log in with Google
+        </button>
         <div class="separator">or</div>
- 
 
-        
         <input
-        placeholder="Email..."
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password..."
-        onChange={(e) => setPassword(e.target.value)}
-      />
-       <button onClick={signIn}>Sign In</button>
-       
-       <hr></hr>
-      </div>
-      
-      <>
-     <MDBBtn onClick={toggleShow}>Create new account</MDBBtn>
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button class="sign-in-btn" onClick={signIn}>
+          Sign in
+        </button>
 
-      <MDBModal tabIndex='-1' show={centredModal} setShow={setCentredModal}>
-        <MDBModalDialog centered>
-          <MDBModalContent>
-            <MDBModalHeader>
-              <MDBModalTitle>Sign up</MDBModalTitle>
-              <MDBBtn className='btn-close' color='none' onClick={toggleShow}></MDBBtn>
-            </MDBModalHeader>
-            <MDBModalBody>
-               <input
-        placeholder="Email..."
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password..."
-        onChange={(e) => setPassword(e.target.value)}
-      />
-            </MDBModalBody>
-            <MDBModalFooter>
-              <MDBBtn color='secondary' onClick={toggleShow}>
-                Cancel
-              </MDBBtn>
-              <MDBBtn onClick={() => {
-                signUp()
-                toggleShow()
-              }}>Sign up</MDBBtn>
-            </MDBModalFooter>
-          </MDBModalContent>
-        </MDBModalDialog>
-      </MDBModal>
+        <hr></hr>
+        <>
+        <button className="sign-up-btn" onClick={toggleShow}>Create new account</button>
+        {/* <button onClick={() => console.log('current user', auth.currentUser)}>
+          Check user
+        </button>
+        <button onClick={logOut}>Log out</button> */}
+
+        <MDBModal tabIndex="-1" show={centredModal} setShow={setCentredModal}>
+          <MDBModalDialog centered>
+            <MDBModalContent>
+              <MDBModalHeader>
+                <MDBModalTitle>Sign up</MDBModalTitle>
+                <MDBBtn
+                  className="btn-close"
+                  color="none"
+                  onClick={toggleShow}
+                ></MDBBtn>
+              </MDBModalHeader>
+              <MDBModalBody>
+                <input
+                  placeholder="Email..."
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                  type="password"
+                  placeholder="Password..."
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </MDBModalBody>
+              <MDBModalFooter>
+                <MDBBtn color="secondary" onClick={toggleShow}>
+                  Cancel
+                </MDBBtn>
+                <MDBBtn
+                
+                  onClick={() => {
+                    signUp();
+                    toggleShow();
+                  }}
+                >
+                  Sign up
+                </MDBBtn>
+              </MDBModalFooter>
+            </MDBModalContent>
+          </MDBModalDialog>
+        </MDBModal>
       </>
+      </div>
+
+      
       {/* <h1>Welcome Back!</h1>
       <input
         placeholder="Email..."
@@ -137,7 +156,6 @@ const Auth = () => {
       <button onClick={logOut}>Log out</button>
       <button onClick={() => console.log('current user', auth.currentUser)}>Check user</button> */}
     </div>
-    
   );
 };
 
